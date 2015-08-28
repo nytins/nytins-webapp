5d8253fdafb1219023d9b48f1f46f92191f67e1f
Nithin Satheesan <nytins@gmail.com>
2015-08-27 21:42:33 -0400

    Move dispatching config to web.xml.

--- src/main/java/com/nytins/web/DispatcherServlet.java
+++ src/main/java/com/nytins/web/DispatcherServlet.java
@@ -16,31 +16,17 @@ public class DispatcherServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Recieved request " + request.getRequestURI());
 		
-		System.out.println("Real path: " + getServletContext().getRealPath("/"));
-		
-		String page;
-		switch(request.getRequestURI()) {
-		case "/pages/left":
-			page = "/left-sidebar.html";
-			break;
-		case "/pages/right":
-			page = "/right-sidebar.html";
-			break;
-		case "/pages/contact-me":
-			page = "/contact-me.jsp";
-			break;
-		case "/":
-		case "/pages/home":
-		default:
-			page = "/index.html";
-			break;
-		}
-		logUserActivity(request.getRemoteAddr(), getServletContext().getRealPath("/"), page);
+		String page = getInitParameter(request.getRequestURI());
+		System.out.println("Page to dispatch to: " + page);
+
+		logUserActivity(request.getRemoteAddr(), page);
 		request.getRequestDispatcher(page).forward(request, response);
 	}
 
-	private void logUserActivity(String ipAddress, String realPath, String page) throws IOException {
-		try(FileWriter writer = new FileWriter(realPath + "/user-activity.log", true)) {
+	private void logUserActivity(String ipAddress, String page) throws IOException {
+		String logDir = getServletContext().getRealPath("/");
+		
+		try(FileWriter writer = new FileWriter(logDir + "/user-activity.log", true)) {
 			writer.write(new Date().toString() + "," + ipAddress + "," + page + "\n");
 		}
 	}
